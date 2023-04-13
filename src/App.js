6359a917d8bd72788b93a0e2bd37f1b0720d7a37
Lionen89 <Lionen89@yandex.ru>
2023-04-13 15:37:26 +0300

    feat/create sceleton of app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-enterprise';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
